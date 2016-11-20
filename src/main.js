7d991eba351b8d6fd0e4783a3e225465fba29736
songlairui <cnre@live.cn>
2016-11-20 10:20:04 +0800

    vue init webpack with test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  template: '<App/>',
+  components: { App }
 })
