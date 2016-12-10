2a4c1d52d11179336b506027b9111a45bc81860e
yaoxiaoge <yaoxiaogehw@gmail.com>
2016-12-10 15:42:39 +0800

    增加了已开发和已发表的页面内容
    增加了已开发和已发表的页面内容

--- src/pages/user/list/proj-pub.vue
+++ src/pages/user/list/proj-pub.vue
@@ -1,6 +1,172 @@
 <template>
   <main-layout>
-    <p>我发布的项目</p>
+    <div class="container">
+        <div class="row">
+            <h2>我发布的项目</h2>
+            <hr>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-3">
+                <div class="thumbnail item">
+                    <a href=""><img data-src="holder.js/300x300"/></a>
+                    <div class="caption">
+                        <h4><a href="#">项目名称</a></h4>
+                        <div class="intro">
+                            <p>项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-comment"></span></a>
+                                <span>355</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-wrench"></span></a>
+                                <span>234</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-eye-open"></span></a>
+                                <span>334</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+                                <span>456</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-3">
+                <div class="thumbnail item">
+                    <a href=""><img data-src="holder.js/300x300"></a>
+                    <div class="caption">
+                        <h4><a href="#">项目名称</a></h4>
+                        <div class="intro">
+                            <p>项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-comment"></span></a>
+                                <span>355</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-wrench"></span></a>
+                                <span>234</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-eye-open"></span></a>
+                                <span>334</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+                                <span>456</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-3">
+                <div class="thumbnail item">
+                    <a href=""><img data-src="holder.js/300x300"></a>
+                    <div class="caption">
+                        <h4><a href="#">项目名称</a></h4>
+                        <div class="intro">
+                            <p>项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-comment"></span></a>
+                                <span>355</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-wrench"></span></a>
+                                <span>234</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-eye-open"></span></a>
+                                <span>334</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+                                <span>456</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-3">
+                <div class="thumbnail item">
+                    <a href=""><img data-src="holder.js/300x300"></a>
+                    <div class="caption">
+                        <h4><a href="#">项目名称</a></h4>
+                        <div class="intro">
+                            <p>项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-comment"></span></a>
+                                <span>355</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-wrench"></span></a>
+                                <span>234</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-eye-open"></span></a>
+                                <span>334</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+                                <span>456</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-3">
+                <div class="thumbnail item">
+                    <a href=""><img data-src="holder.js/300x300"></a>
+                    <div class="caption">
+                        <h4><a href="#">项目名称</a></h4>
+                        <div class="intro">
+                            <p>项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-comment"></span></a>
+                                <span>355</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-wrench"></span></a>
+                                <span>234</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-eye-open"></span></a>
+                                <span>334</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-3 col-md-3">
+                                <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+                                <span>456</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <hr>
+            <nav class="text-center">
+                <ul class="pagination pagination-lg">
+                    <li class="previous disabled"><a href="#">← </a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li class="next"><a href="#">→</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
   </main-layout>
 </template>
 
@@ -13,3 +179,45 @@
     }
   }
 </script>
+
+<style>
+	body{
+    	background-color: #fefef5;
+    	font-family: "Microsoft YaHei", sans-serif;
+        }
+        
+    .item{
+        overflow: hidden;
+        box-sizing: border-box;
+        background: rgba(0, 0, 0, .05);
+        padding: 10px;
+        margin: 10px 0;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .3);
+        border-radius: 4px;
+        }
+
+    .item img {
+        display: block;
+        width: 100%;
+        margin: 0 auto;
+        min-height: 150px;
+        background-color: aqua;
+        }
+
+    .item .intro{
+        font-size: 14px;
+        }
+
+    .item h4{
+        font-size: 19px;
+        }
+
+    .item h4 a{
+        text-decoration: none;
+        color: #000000;
+       	}
+
+    .item h4 a:hover{
+        color: darkcyan;
+        }
+</style>
